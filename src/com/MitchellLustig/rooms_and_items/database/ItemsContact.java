f2fbcb722cfbd8006ac8f5b203f97d6582ac7bc2
Mitch Lustig <moesk8er2@gmail.com>
2014-10-08 17:50:16 -0400

    Adds items contract

---
+++ src/com/MitchellLustig/rooms_and_items/database/ItemsContact.java
@@ -0,0 +1,16 @@
+package com.MitchellLustig.rooms_and_items.database;
+
+import android.provider.BaseColumns;
+
+public class ItemsContact {
+
+	public ItemsContact() {}
+	
+	public static abstract class RoomEntry implements BaseColumns {
+		public static final String WORLD_ID = "worldid";
+		public static final String ROOM_X = "roomx";
+		public static final String ROOM_Y = "roomy";
+		public static final String ROOM_NAME = "roomname";
+	}
+
+}
