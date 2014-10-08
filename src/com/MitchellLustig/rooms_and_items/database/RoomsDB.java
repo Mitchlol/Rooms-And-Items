8c120f66cbe482c989b21b58ffa7ac21e67a69fe
Mitch Lustig <moesk8er2@gmail.com>
2014-10-08 18:14:15 -0400

    Refactors roomscontract and moves it into roomsdb

--- src/com/MitchellLustig/rooms_and_items/database/RoomsDB.java
+++ src/com/MitchellLustig/rooms_and_items/database/RoomsDB.java
@@ -6,9 +6,20 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import com.MitchellLustig.rooms_and_items.database.RoomsContact.RoomEntry;
-
 public class RoomsDB extends SQLiteOpenHelper {
+	
+	public static final class SCHEMA{
+		public static final String DATABASE = "roomsanditems";
+		public static final class Tables{
+			public static final String ROOMS = "rooms";
+			public static final class Rooms{
+				public static final String WORLD_ID = "worldid";
+				public static final String ROOM_X = "roomx";
+				public static final String ROOM_Y = "roomy";
+				public static final String ROOM_NAME = "roomname";
+			}
+		}
+	}
 
 	public RoomsDB(Context context, String name, CursorFactory factory, int version) {
 		super(context, name, factory, version);
