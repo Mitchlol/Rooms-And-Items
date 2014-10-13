9606e7a39842b20cd3b41775bf9b20fc73292f8f
Mitch Lustig <moesk8er2@gmail.com>
2014-10-12 20:18:15 -0400

    Adds move functionality to controller

--- src/com/MitchellLustig/rooms_and_items/game/GameController.java
+++ src/com/MitchellLustig/rooms_and_items/game/GameController.java
@@ -15,6 +15,11 @@ public class GameController {
 		db = new RoomsAndItemsDB(context);
 	}
 	//Simplified user functions for only one user
+	public String getCurrentUserId(){
+		Cursor cursor = db.getUsers();
+		cursor.moveToFirst();
+		return cursor.getString(cursor.getColumnIndex(Schema.Tables.Users._ID));
+	}
 	public String getCurrentUserName(){
 		Cursor cursor = db.getUsers();
 		cursor.moveToFirst();
@@ -26,6 +31,12 @@ public class GameController {
 		return cursor.getString(cursor.getColumnIndex(Schema.Tables.Users.LOCATION));
 	}
 	
+	public String getCurrentRoomName(){
+		Cursor cursor = db.getRooms();
+		cursor.moveToFirst();
+		return cursor.getString(cursor.getColumnIndex(Schema.Tables.Rooms.ROOM_NAME));
+	}
+	
 	public Point getRoomLocation(String roomId){
 		Cursor cursor = db.getRoom(roomId);
 		cursor.moveToFirst();
@@ -34,10 +45,78 @@ public class GameController {
 		return new Point(roomX, roomY);
 	}
 	
-	public String getCurrentRoomInfo(){
-		Cursor cursor = db.getRooms();
-		cursor.moveToFirst();
-		return cursor.getString(cursor.getColumnIndex(Schema.Tables.Rooms.ROOM_NAME));
+	public String getRoomUp(String roomId){
+		Point room = getRoomLocation(roomId);
+		Cursor cursor =  db.getRoom(room.x, room.y+1);
+		if(cursor.getCount() == 0){
+			return null;
+		}else{
+			cursor.moveToFirst();
+			return cursor.getString(cursor.getColumnIndex(Schema.Tables.Rooms._ID));
+		}
+	}
+	public String getRoomDown(String roomId){
+		Point room = getRoomLocation(roomId);
+		Cursor cursor =  db.getRoom(room.x, room.y-1);
+		if(cursor.getCount() == 0){
+			return null;
+		}else{
+			cursor.moveToFirst();
+			return cursor.getString(cursor.getColumnIndex(Schema.Tables.Rooms._ID));
+		}
+	}
+	public String getRoomRight(String roomId){
+		Point room = getRoomLocation(roomId);
+		Cursor cursor =  db.getRoom(room.x+1, room.y);
+		if(cursor.getCount() == 0){
+			return null;
+		}else{
+			cursor.moveToFirst();
+			return cursor.getString(cursor.getColumnIndex(Schema.Tables.Rooms._ID));
+		}
+	}
+	public String getRoomLeft(String roomId){
+		Point room = getRoomLocation(roomId);
+		Cursor cursor =  db.getRoom(room.x-1, room.y);
+		if(cursor.getCount() == 0){
+			return null;
+		}else{
+			cursor.moveToFirst();
+			return cursor.getString(cursor.getColumnIndex(Schema.Tables.Rooms._ID));
+		}
+	}
+	
+	public boolean moveUp(){
+		String up = getRoomUp(getCurrentUserLocation());
+		if(up == null){
+			return false;
+		}else{
+			return db.setUserLocation(getCurrentUserId(), up) > 0 ? true : false;
+		}
+	}
+	public boolean moveDown(){
+		String down = getRoomDown(getCurrentUserLocation());
+		if(down == null){
+			return false;
+		}else{
+			return db.setUserLocation(getCurrentUserId(), down) > 0 ? true : false;
+		}
+	}
+	public boolean moveRight(){
+		String right = getRoomRight(getCurrentUserLocation());
+		if(right == null){
+			return false;
+		}else{
+			return db.setUserLocation(getCurrentUserId(), right) > 0 ? true : false;
+		}
+	}
+	public boolean moveLeft(){
+		String left = getRoomLeft(getCurrentUserLocation());
+		if(left == null){
+			return false;
+		}else{
+			return db.setUserLocation(getCurrentUserId(), left) > 0 ? true : false;
+		}
 	}
 	
 	public void close(){
